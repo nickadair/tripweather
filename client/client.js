3fdd89ed4a7c87044179f8f5c533fe8fe13a72ad
Nick Adair <nick.adair@live.com>
2015-09-19 03:21:20 +0000

    Refactoring

---
+++ client/client.js
@@ -0,0 +1,65 @@
+Meteor.startup(function() {
+		Session.setDefault('zipEntered', false);
+  });
+
+	Template.mainContent.helpers({
+		isLocationKnown: function() { return Session.get('zipEntered') }
+	});
+
+  Template.zipInput.events({
+    'submit form': function(event) {
+      event.preventDefault();
+		  Session.set('zipEntered', true);
+		  Session.set('cityState1', document.getElementById('cityState1').value);
+		  Session.set('cityState2', document.getElementById('cityState2').value);
+      GoogleMaps.load();
+    }
+  })
+	Template.map.helpers({
+	  mainMapOptions: function() {
+	    if (GoogleMaps.loaded()) {
+	      return {
+			    center: new google.maps.LatLng(39.828127, -98.579404),
+			    zoom: 4
+	      };
+	    }
+	  }
+	});
+
+	Template.map.onCreated(function() {
+		GoogleMaps.ready('mainMap', function(map) {
+		  var cityState1 = Session.get('cityState1');
+		  var cityState2 = Session.get('cityState2');
+	    var loc1 = null;
+		  var loc2 = null;
+		  geocoder = new google.maps.Geocoder();
+	  	geocoder.geocode( { 'address': cityState1}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          loc1 = results[0].geometry.location;
+        } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
+	  	});
+	    geocoder.geocode( { 'address': cityState2}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          loc2 = results[0].geometry.location;
+          var bounds_1 = new google.maps.LatLng(loc1.lat(), loc1.lng());
+          var bounds_2 = new google.maps.LatLng(loc2.lat(), loc2.lng());
+          var bounds = new google.maps.LatLngBounds();
+          bounds.extend(bounds_1);
+          bounds.extend(bounds_2);
+          map.instance.fitBounds(bounds);
+          var marker1 = new google.maps.Marker({
+            map: map.instance,
+            position: loc1
+          });
+          var marker2 = new google.maps.Marker({
+            map: map.instance,
+            position: loc2
+          });
+        } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
+	  	});
+	  });
+	});
